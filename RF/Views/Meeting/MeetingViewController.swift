a9fb03c8bec20aef9607effcee9d9744e83dc8f6
JJ <jeonghojin03@gmail.com>
2023-07-06 20:29:19 +0900

    feat: init meeting view

---
+++ RF/Views/Meeting/MeetingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MeetingViewController.swift
+//  RF
+//
+//  Created by 정호진 on 2023/07/06.
+//
+
+import Foundation
+import SnapKit
+import UIKit
+import RxSwift
+
+final class MeetingViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
