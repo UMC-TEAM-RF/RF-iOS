0d09e456737c593008070db81c3b549d1043b5fe
Jeong Dong <eejxdong99@gmail.com>
2023-07-11 11:19:00 +0900

    feat: 모임 생성 화면(세부 정보) 일부 구현

--- RF/Views/Meeting/Creating/SetDetailInfoViewController.swift
+++ RF/Views/Meeting/Creating/SetDetailInfoViewController.swift
@@ -9,11 +9,107 @@ import UIKit
 import SnapKit
 
 class SetDetailInfoViewController: UIViewController {
+    
+    // MARK: - UI Property
+    
+    // 네비게이션 바
+    private lazy var navigationBar: CreateMeetingNavigationBar = {
+        let view = CreateMeetingNavigationBar()
+        view.delegate = self
+        return view
+    }()
+    
+    // 프로그레스 바
+    private lazy var progressBar: UIProgressView = {
+        let pv = UIProgressView()
+        pv.progressViewStyle = .bar
+        pv.backgroundColor = UIColor(hexCode: "D1D1D1")
+        pv.progress = 1
+        return pv
+    }()
+    
+    // 메인 라벨
+    private lazy var mainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "모임의 세부 정보를 입력해 주세요."
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        return label
+    }()
+    
+    // 다음 버튼
+    private lazy var createButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("모임 생성하기", for: .normal)
+        button.backgroundColor = .tintColor
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        
+        addSubviews()
+        configureConstraints()
+        addTargets()
     }
     
+    // MARK: - addSubviews
+    
+    private func addSubviews() {
+        view.addSubview(navigationBar)
+        view.addSubview(progressBar)
+        view.addSubview(mainLabel)
+        view.addSubview(createButton)
+    }
+    
+    // MARK: - configureConstraints
+    
+    private func configureConstraints() {
+        // 네비게이션 바
+        navigationBar.snp.makeConstraints { make in
+            make.top.left.right.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(60)
+        }
+        
+        // 프로그레스 바
+        progressBar.snp.makeConstraints { make in
+            make.top.equalTo(navigationBar.snp.bottom)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(25)
+        }
+        
+        // 메인 라벨
+        mainLabel.snp.makeConstraints { make in
+            make.top.equalTo(progressBar.snp.bottom).offset(25)
+            make.centerX.equalToSuperview()
+        }
+        
+        // 다음
+        createButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(30)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-30)
+            make.height.equalTo(50)
+        }
+    }
+    
+    // MARK: - addTargets
+    
+    private func addTargets() {
+        createButton.addTarget(self, action: #selector(createMeetingTapped), for: .touchUpInside)
+    }
+    
+    // MARK: - @objc func
+    
+    @objc private func createMeetingTapped() {
+        tabBarController?.tabBar.isHidden = false
+        navigationController?.popToRootViewController(animated: true)
+    }
+}
+
+extension SetDetailInfoViewController: NavigationBarDelegate {
+    func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
 }
