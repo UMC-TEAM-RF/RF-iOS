21a8c932cd7b2d6e6bdb4f0503b3996b6013f4c6
Jeong Dong <eejxdong99@gmail.com>
2023-07-23 15:58:55 +0900

    feat: 알림 리스트 화면 구현

--- RF/Views/Home/NotiMessageViewController.swift
+++ RF/Views/Home/NotiMessageViewController.swift
@@ -19,7 +19,22 @@ class NotiMessageViewController: UIViewController {
         return bar
     }()
     
+    private lazy var notiListTableView: UITableView = {
+        let tb = UITableView()
+        tb.dataSource = self
+        tb.delegate = self
+        tb.register(NotiListTableViewCell.self, forCellReuseIdentifier: NotiListTableViewCell.identifier)
+        tb.rowHeight = 70
+        tb.separatorStyle = .none
+        return tb
+    }()
+    
     // MARK: - Property
+    
+    let dummy: [String] = [
+        "kimhim 님이 '마라탕 맛집 탐방'에 참여하기를 원해요!",
+        "ljdongz 님이 '디천: 디자인 천재들 모임'에 참여하기를 원해요!",
+    ]
     
     // MARK: - viewDidLoad()
     
@@ -36,6 +51,7 @@ class NotiMessageViewController: UIViewController {
     
     private func addSubviews() {
         view.addSubview(navigationBar)
+        view.addSubview(notiListTableView)
     }
     
     // MARK: - configureConstraints()
@@ -45,12 +61,39 @@ class NotiMessageViewController: UIViewController {
             make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
             make.height.equalTo(60)
         }
+        
+        notiListTableView.snp.makeConstraints { make in
+            make.top.equalTo(navigationBar.snp.bottom)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 
 }
+
+// MARK: - Ext: NavigationBarDelegate
 
 extension NotiMessageViewController: NavigationBarDelegate {
     func backButtonTapped() {
         self.navigationController?.popViewController(animated: true)
     }
 }
+
+// MARK: - Ext: TableView
+
+extension NotiMessageViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummy.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NotiListTableViewCell.identifier, for: indexPath) as? NotiListTableViewCell else { return UITableViewCell() }
+        
+        cell.notiLabelText = dummy[indexPath.row]
+        cell.timeLabelText = "23시간"
+        
+        return cell
+    }
+}
+
+
