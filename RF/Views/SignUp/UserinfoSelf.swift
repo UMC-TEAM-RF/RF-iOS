c2f2add5ee461dd9c940d9a025882e7b97eb434a
ya_e_un <nyeshinee0525@naver.com>
2023-07-26 17:24:26 +0900

    feat: 유저 한 줄 소개 수정

--- RF/Views/SignUp/UserinfoSelf.swift
+++ RF/Views/SignUp/UserinfoSelf.swift
@@ -3,6 +3,7 @@
 //  RF
 //
 //  Created by 나예은 on 2023/07/19.
+//.textColor = UIColor(hexCode: "#3C3A3A") / UIColor(hexCode: "#A0A0A0")
 //
 
 import UIKit
@@ -12,81 +13,109 @@ class UserinfoSelf: UIViewController {
     
     private lazy var topLabel: UILabel = {
         let label = UILabel()
-        label.text = "본인 한 줄 소개를 해주세요."
+        label.text = "한 줄 소개"
         label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-        label.numberOfLines = 2
+        label.textColor = UIColor(hexCode: "#3C3A3A")
+        label.numberOfLines = 1
         return label
     }()
-
-    private lazy var keywordLabel: UILabel = {
+    
+    private lazy var subLabel: UILabel = {
         let label = UILabel()
-        label.text = "나를 나타낼 수 있는 대표적 키워드를 적어주세요."
-        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.text = "  " + "나를 나타낼 수 있는 대표적 키워드를 적어주세요."
+        label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        label.textColor = UIColor(hexCode: "#3C3A3A")
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private lazy var textField: UITextField = {
+        let field = UITextField()
+        field.borderStyle = .roundedRect
+        field.borderStyle = .none
+        field.layer.cornerRadius = 5
+        field.placeholder = "   " + "한 줄 소개를 작성해주세요!"
+        field.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        field.backgroundColor = .clear
+        field.textColor = UIColor(hexCode: "#A0A0A0")
+        let bottomLine = UIView()
+        bottomLine.backgroundColor = .gray
+        field.addSubview(bottomLine)
+        bottomLine.translatesAutoresizingMaskIntoConstraints = false
+        bottomLine.leadingAnchor.constraint(equalTo: field.leadingAnchor).isActive = true
+        bottomLine.trailingAnchor.constraint(equalTo: field.trailingAnchor).isActive = true
+        bottomLine.bottomAnchor.constraint(equalTo: field.bottomAnchor).isActive = true
+        bottomLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        return field
+        }()
+
+    private lazy var warningLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이후에 변경할 수 없으니 신중히 결정해주세요!"
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.textColor = UIColor(hexCode: "#A0A0A0")
         label.numberOfLines = 1
         return label
     }()
 
+    // 이미지 왼쪽 화살표
+    private lazy var imageView: UIImageView = {
+            let imageView = UIImageView(frame: CGRect(x: 50, y: 100, width: 200, height: 200))
+            imageView.contentMode = .scaleAspectFit
+            if let image = UIImage(named: "leftArrow") {
+                imageView.image = image
+            }
+            return imageView
+        }()
     
     private lazy var nextButton: UIButton = {
-        
-        let UserSelectSchoolViewController = UserSelectSchoolViewController()
-        navigationController?.pushViewController(UserSelectSchoolViewController, animated: true)
-        
         let button = UIButton()
         button.setTitle("다음", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        button.setTitleColor(UIColor(hexCode: "#3C3A3A") , for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .regular)
         button.backgroundColor =  UIColor(hexCode: "#F5F5F5")
         button.layer.cornerRadius = 5
         return button
     }()
 
-    
-    private var textField: UITextField = {
-        let field = UITextField()
-        field.borderStyle = .roundedRect
-        field.borderStyle = .none
-        field.placeholder = "   한 줄 소개를 작성해주세요!"
-        field.backgroundColor = UIColor(hexCode: "#F5F5F5")
-        field.textColor = UIColor(hexCode: "#818181")
-        field.layer.cornerRadius = 5
-        return field
-    }()
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
         view.backgroundColor = .systemBackground
-        
         addSubviews()
         configureConstraints()
     }
     
     
     private func addSubviews() {
-        view.addSubview(topLabel)
-        view.addSubview(keywordLabel)
-        view.addSubview(nextButton)
-        view.addSubview(textField)
-       
+        view.addSubview(imageView) // 왼쪽 화살표 이미지
+        view.addSubview(topLabel) // 한 줄 소개
+        view.addSubview(subLabel) // 나를 나타낼 수 있는 대표적 키워드를 적어주세요
+        view.addSubview(textField) // 한 줄 소개를 작성해주세요!
+        view.addSubview(warningLabel) // 이후에 변경할 수 없으니 신중히 결정해주세요!
+        view.addSubview(nextButton) // 다음
     }
     
     private func configureConstraints() {
         
+        // 왼쪽 화살표 이미지
+        imageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(17)
+            make.leading.equalToSuperview().inset(10)
+        }
+        
+        //한 줄 소개
         topLabel.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).inset(25)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.leading.trailing.equalToSuperview().inset(44)
+        }
+        
+        // 나를 나타낼 수 있는 대표적 키워드를 적어주세요
+        subLabel.snp.makeConstraints { make in
+            make.top.equalTo(topLabel.snp.bottom).offset(25)
             make.leading.trailing.equalToSuperview().inset(20)
         }
         
-        keywordLabel.snp.makeConstraints { make in
-            make.top.equalTo(topLabel.snp.bottom).offset(25)
-            make.trailing.equalToSuperview().inset(-20)
-            make.leading.equalToSuperview().inset(20)
-        }
-        
-
+        // 한 줄 소개를 작성해주세요!
         textField.snp.makeConstraints { make in
             make.top.equalTo(topLabel.snp.bottom).offset(55)
             make.leading.equalToSuperview().inset(20)
@@ -94,16 +123,20 @@ class UserinfoSelf: UIViewController {
             make.height.equalTo(50)
         }
         
+        // 이후에 변경할 수 없으니 신중히 결정해주세요!
+        warningLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(nextButton.snp.top).offset(-20)
+            make.leading.trailing.equalToSuperview().inset(70)
+        }
+    
         //다음
         nextButton.snp.makeConstraints { make in
             make.leading.right.equalToSuperview().inset(30)
             make.bottom.equalToSuperview().inset(50)
             make.height.equalTo(48)
         }
-
+        
     }
-    
-    
 }
 
 
