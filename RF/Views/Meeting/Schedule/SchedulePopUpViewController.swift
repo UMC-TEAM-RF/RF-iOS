9ca9ec341fe647e4754b4e7dbd791cd4c566d0e7
JJ <jeonghojin03@gmail.com>
2023-07-29 00:15:21 +0900

    feat: 모임 일정 관리 알림 창 구현

--- RF/Views/Meeting/Schedule/SchedulePopUpViewController.swift
+++ RF/Views/Meeting/Schedule/SchedulePopUpViewController.swift
@@ -11,13 +11,127 @@ import SnapKit
 import RxSwift
 
 /// MARK: 일정을 눌렀을 때 뜨는 팝업 창
-final class SchedulePopUpViewController: DimmedViewController{
+final class SchedulePopUpViewController: DimmedViewController {
     
     /// MARK: Pop up 창 base view
     private lazy var baseView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
-        view.layer.cornerRadius = 20
+        view.layer.cornerRadius = 30
+        return view
+    }()
+    
+    /// MARK: 모임 별명
+    private lazy var meetingNickname: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.text = "aaad"
+        return label
+    }()
+    
+    /// MARK: 모임 이름
+    private lazy var meetingName: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15)
+        label.text = "aaa"
+        return label
+    }()
+    
+    /// MARK: 날짜 제목
+    private lazy var dateTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.text = SchedulePopUp.date
+        return label
+    }()
+    
+    /// MARK: 이벤트 날짜
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        return label
+    }()
+    
+    /// MARK: 날짜 Label들 담는 view
+    private lazy var dateUIView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    /// MARK: 시간 제목
+    private lazy var timeTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.text = SchedulePopUp.time
+        return label
+    }()
+    
+    /// MARK: 이벤트 시간
+    private lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        return label
+    }()
+    
+    /// MARK: 시간 Label들 담는 view
+    private lazy var timeUIView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    
+    /// MARK: 장소 제목
+    private lazy var placeTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.text = SchedulePopUp.place
+        return label
+    }()
+    
+    /// MARK: 이벤트 장소
+    private lazy var placeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        return label
+    }()
+    
+    /// MARK: 장소 Label들 담는 view
+    private lazy var placeUIView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    
+    /// MARK: 인원 제목
+    private lazy var peopleTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.text = SchedulePopUp.people
+        return label
+    }()
+
+    /// MARK: 이벤트 인원수
+    private lazy var peopleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        return label
+    }()
+    
+    /// MARK: 인원 Label들 담는 view
+    private lazy var peopleUIView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    /// MARK: 날짜, 시간, 장소, 인원 View 담는 stackview
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [dateUIView, timeUIView, placeUIView, peopleUIView])
+        view.axis = .vertical
+        view.distribution = .fillEqually
         return view
     }()
     
@@ -28,6 +142,7 @@ final class SchedulePopUpViewController: DimmedViewController{
     init() {
         super.init(durationTime: 0.3, alpha: 0.25)
         addSubviews()
+        getData()
     }
     
     required init?(coder: NSCoder) {
@@ -35,9 +150,28 @@ final class SchedulePopUpViewController: DimmedViewController{
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        dismiss(animated: true)
+    }
+    
     /// MARK: Add UI
     private func addSubviews(){
         view.addSubview(baseView)
+        baseView.addSubview(meetingNickname)
+        baseView.addSubview(meetingName)
+        baseView.addSubview(stackView)
+        
+        dateUIView.addSubview(dateLabel)
+        dateUIView.addSubview(dateTitleLabel)
+        
+        timeUIView.addSubview(timeLabel)
+        timeUIView.addSubview(timeTitleLabel)
+        
+        placeUIView.addSubview(placeLabel)
+        placeUIView.addSubview(placeTitleLabel)
+        
+        peopleUIView.addSubview(peopleLabel)
+        peopleUIView.addSubview(peopleTitleLabel)
         
         configureConstraints()
     }
@@ -48,9 +182,67 @@ final class SchedulePopUpViewController: DimmedViewController{
             make.centerY.equalToSuperview()
             make.leading.equalToSuperview().offset(30)
             make.trailing.equalToSuperview().offset(-30)
-            make.height.equalTo(view.safeAreaLayoutGuide.layoutFrame.height/3)
+            make.height.equalTo(view.safeAreaLayoutGuide.layoutFrame.height/4)
         }
+        
+        meetingNickname.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.equalTo(baseView.snp.leading).offset(20)
+            make.trailing.equalTo(baseView.snp.trailing).offset(-20)
+        }
+        
+        meetingName.snp.makeConstraints { make in
+            make.top.equalTo(meetingNickname.snp.bottom).offset(10)
+            make.leading.equalTo(baseView.snp.leading).offset(20)
+            make.trailing.equalTo(baseView.snp.trailing).offset(-20)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(meetingName.snp.bottom).offset(20)
+            make.leading.equalTo(baseView.snp.leading).offset(20)
+            make.trailing.equalTo(baseView.snp.trailing).offset(-20)
+            make.height.equalTo(baseView.snp.height).multipliedBy(0.5)
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+        }
+        
+        dateTitleLabel.snp.makeConstraints { make in
+            make.top.bottom.leading.equalToSuperview()
+        }
+        
+        timeLabel.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+        }
+        
+        timeTitleLabel.snp.makeConstraints { make in
+            make.top.bottom.leading.equalToSuperview()
+        }
+        
+        placeLabel.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+        }
+        
+        placeTitleLabel.snp.makeConstraints { make in
+            make.top.bottom.leading.equalToSuperview()
+        }
+        
+        peopleLabel.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+        }
+        
+        peopleTitleLabel.snp.makeConstraints { make in
+            make.top.bottom.leading.equalToSuperview()
+        }
+        
     }
     
-    
+    /// MARK: test Data
+    private func getData(){
+        dateLabel.text = "ㅁㄴㅇㄹㄹㄴ"
+        timeLabel.text = "ㅁㄴㅇㄹㄹㄴ"
+        placeLabel.text = "ㅁㄴㅇㄹㄹㄴ"
+        peopleLabel.text = "ㅁㄴㅇㄹㄹㄴ"
+    }
 }
