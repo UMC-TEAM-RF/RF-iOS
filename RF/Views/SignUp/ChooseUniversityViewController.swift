a62fd9b22469c4adf1f34c1f59d08cb76a9d4aaf
JJ <jeonghojin03@gmail.com>
2023-08-05 19:05:21 +0900

    feat: 학교 선택하는 화면 init

---
+++ RF/Views/SignUp/ChooseUniversityViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CertificatedUniversity.swift
+//  RF
+//
+//  Created by 정호진 on 2023/08/05.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+/// 학교 선택하는 화면
+final class ChooseUniversityViewController: UIViewController {
+    
+    
+    
+    
+    // MARK: - View Did Load
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
