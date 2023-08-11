c07ba25aedd7182438a05cade0f09118ab621000
ya_e_un <nyeshinee0525@naver.com>
2023-08-11 21:07:52 +0900

    feat : 마이페이지 설정

--- RF/Views/Setting/ProfileSettingViewController.swift
+++ RF/Views/Setting/ProfileSettingViewController.swift
@@ -12,7 +12,7 @@ import SnapKit
 class ProfileSettingViewController: UIViewController {
 
     var tableView: UITableView!
-    let data = ["프리미엄 이용권", "공지사항", "고객센터 / 도움말", "버전 정보", "차단 관리", "회원 탈퇴"]
+    let data = ["  프리미엄 이용권", "  공지사항", "  고객센터 / 도움말", "  버전 정보", "  차단 관리", "  회원 탈퇴"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +20,8 @@ class ProfileSettingViewController: UIViewController {
     }
     
     private func setupUI() {
-        setupSomeView() // someView 먼저 배치
-        setupTableView() // 그 다음 테이블 뷰 배치
+        setupSomeView()
+        setupTableView()
     }
     
     private func setupTableView() {
@@ -32,7 +32,7 @@ class ProfileSettingViewController: UIViewController {
         
         view.addSubview(tableView)
         tableView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(80) // someView 아래로 배치
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(80)
             make.leading.trailing.bottom.equalToSuperview()
         }
     }
@@ -44,9 +44,12 @@ class ProfileSettingViewController: UIViewController {
         let label = UILabel()
         label.text = "설정"
         label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
         
         let button = UIButton(type: .system)
-        button.setTitle("Click Me", for: .normal)
+        let arrowImage = UIImage(systemName: "carat.png")
+        button.setImage(arrowImage, for: .normal)
+        
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         
         view.addSubview(someView)
@@ -55,19 +58,21 @@ class ProfileSettingViewController: UIViewController {
         
         someView.snp.makeConstraints { make in
             make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            make.height.equalTo(70) // 뷰 높이 조정
+            make.height.equalTo(80) // 뷰 높이 조정
         }
         
         label.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(20)
-            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(44)
+            make.top.bottom.equalToSuperview()
         }
         
         button.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().offset(-20)
-            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(20)
+            make.centerY.equalTo(label)
+            make.width.height.equalTo(24)
         }
     }
+
     
     @objc private func buttonTapped() {
         // 버튼 클릭 시 동작할 내용 추가
@@ -82,19 +87,51 @@ extension ProfileSettingViewController: UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = data[indexPath.row]
         cell.backgroundColor = .clear
         
-        // Add arrow button to each cell
+        let titleLabel = UILabel()
+        titleLabel.text = data[indexPath.row]
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        titleLabel.textColor = .black
+        
         let arrowButton = UIButton(type: .custom)
         arrowButton.setImage(UIImage(named: "carat.png"), for: .normal)
         arrowButton.tag = indexPath.row
         arrowButton.addTarget(self, action: #selector(arrowButtonTapped(_:)), for: .touchUpInside)
         
-        cell.accessoryView = arrowButton
+        cell.addSubview(titleLabel)
+        cell.addSubview(arrowButton)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.centerY.equalToSuperview()
+        }
+        
+        arrowButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(20)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(30)
+        }
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .white
+        
+        cell.addSubview(separatorView)
+        separatorView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(1)
+        }
         
         return cell
     }
+
+    @objc private func arrowButtonTapped(_ sender: UIButton) {
+        let rowIndex = sender.tag
+        print("Arrow button tapped for row \(rowIndex)")
+        // 여기서 이미지 버튼을 눌렀을 때 동작할 내용을 추가하세요
+    }
+
+
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 65 // 각 셀의 높이 조정
@@ -108,9 +145,5 @@ extension ProfileSettingViewController: UITableViewDataSource, UITableViewDelega
         return 1
     }
     
-    @objc private func arrowButtonTapped(_ sender: UIButton) {
-        let rowIndex = sender.tag
-        print("Arrow button tapped for row \(rowIndex)")
-        // Handle the arrow button tap action for the specific row
-    }
+
 }
