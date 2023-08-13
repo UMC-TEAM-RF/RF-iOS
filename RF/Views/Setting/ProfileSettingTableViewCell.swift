3eaf05c0b5fa89bd18fa85a5dab10903c59d7187
ya_e_un <nyeshinee0525@naver.com>
2023-08-13 18:55:06 +0900

    feat: 마이페이지 설정 코드 수정

---
+++ RF/Views/Setting/ProfileSettingTableViewCell.swift
@@ -0,0 +1,74 @@
+//
+//  ProfileSettingTableViewCell.swift
+//  RF
+//
+//  Created by 나예은 on 2023/08/12.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileSettingTableViewCell: UITableViewCell {
+    
+    //MARK: - UI Property UI구성
+    private lazy var menuLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        label.textColor = .darkGray
+        label.numberOfLines = 1
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var menubutton: UIButton = {
+        let button = UIButton()
+        if let image = UIImage(named: "carat.png") {
+            button.setBackgroundImage(image, for: .normal)
+        }
+        return button
+    }()
+
+    
+    // MARK: - Property 테이블 뷰 입력
+    static let identifier = "ProfileSettingTableViewCell"
+    
+    var menuLabelText: String? {
+        didSet {
+            self.menuLabel.text = menuLabelText
+        }
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubviews()
+        configureConstraints()
+        selectionStyle = .none
+        
+        layoutIfNeeded()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - addSubviews() 뷰 나타내기
+    private func addSubviews() {
+        contentView.addSubview(menuLabel)
+        contentView.addSubview(menubutton)
+    }
+    
+    // MARK: - configureConstraints() 뷰 제약걸기
+    private func configureConstraints() {
+        menuLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-20)
+            make.leading.equalToSuperview().offset(20)
+        }
+        
+        menubutton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-20)
+        }
+
+        }
+}
--- RF/Views/Setting/ProfileSettingViewController.swift
+++ RF/Views/Setting/ProfileSettingViewController.swift
@@ -10,140 +10,59 @@ import UIKit
 import SnapKit
 
 class ProfileSettingViewController: UIViewController {
-
-    var tableView: UITableView!
-    let data = ["  프리미엄 이용권", "  공지사항", "  고객센터 / 도움말", "  버전 정보", "  차단 관리", "  회원 탈퇴"]
     
+    //MARK: - UI Property UI구성
+    
+    
+    private lazy var menuTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(ProfileSettingTableViewCell.self, forCellReuseIdentifier: ProfileSettingTableViewCell.identifier)
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 60
+        return tableView
+    }()
+    
+    //MARK: - UI Property UI구성
+    let menuList: [String] = [ "    프리미엄 이용권", "    공지사항", "    고객센터 / 도움말", "    버전 정보", "    차단 관리", "    회원 탈퇴"]
+    
+    // MARK: - viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        view.backgroundColor = .white
+        
+        updateTitleView(title: "설정")
+        
+        addSubviews()
+        configureConstraints()
     }
     
-    private func setupUI() {
-        setupSomeView()
-        setupTableView()
+    // MARK: - addsubviews()
+    private func addSubviews() {
+        view.addSubview(menuTableView)
     }
     
-    private func setupTableView() {
-        tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(80)
-            make.leading.trailing.bottom.equalToSuperview()
+    // MARK: - configureConstraints()
+    private func configureConstraints() {
+        menuTableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
         }
-    }
-    
-    private func setupSomeView() {
-        let someView = UIView()
-        someView.backgroundColor = .white // 배경색 설정
-        
-        let label = UILabel()
-        label.text = "설정"
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-        
-        let button = UIButton(type: .system)
-        let arrowImage = UIImage(systemName: "carat.png")
-        button.setImage(arrowImage, for: .normal)
-        
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        
-        view.addSubview(someView)
-        someView.addSubview(label)
-        someView.addSubview(button)
-        
-        someView.snp.makeConstraints { make in
-            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            make.height.equalTo(80) // 뷰 높이 조정
-        }
-        
-        label.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(44)
-            make.top.bottom.equalToSuperview()
-        }
-        
-        button.snp.makeConstraints { make in
-            make.leading.equalToSuperview().inset(20)
-            make.centerY.equalTo(label)
-            make.width.height.equalTo(24)
-        }
-    }
-
-    
-    @objc private func buttonTapped() {
-        // 버튼 클릭 시 동작할 내용 추가
     }
 }
 
-extension ProfileSettingViewController: UITableViewDataSource, UITableViewDelegate {
-    
+extension ProfileSettingViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return menuList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .clear
-        
-        let titleLabel = UILabel()
-        titleLabel.text = data[indexPath.row]
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
-        titleLabel.textColor = .black
-        
-        let arrowButton = UIButton(type: .custom)
-        arrowButton.setImage(UIImage(named: "carat.png"), for: .normal)
-        arrowButton.tag = indexPath.row
-        arrowButton.addTarget(self, action: #selector(arrowButtonTapped(_:)), for: .touchUpInside)
-        
-        cell.addSubview(titleLabel)
-        cell.addSubview(arrowButton)
-        
-        titleLabel.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(20)
-            make.centerY.equalToSuperview()
-        }
-        
-        arrowButton.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().inset(20)
-            make.centerY.equalToSuperview()
-            make.width.height.equalTo(30)
-        }
-        
-        let separatorView = UIView()
-        separatorView.backgroundColor = .white
-        
-        cell.addSubview(separatorView)
-        separatorView.snp.makeConstraints { make in
-            make.leading.trailing.bottom.equalToSuperview()
-            make.height.equalTo(1)
-        }
-        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ProfileSettingTableViewCell.identifier, for: indexPath) as? ProfileSettingTableViewCell else { return UITableViewCell() }
+
+        cell.menuLabelText = "    " + menuList[indexPath.row]
+
         return cell
     }
-
-    @objc private func arrowButtonTapped(_ sender: UIButton) {
-        let rowIndex = sender.tag
-        print("Arrow button tapped for row \(rowIndex)")
-        // 여기서 이미지 버튼을 눌렀을 때 동작할 내용을 추가하세요
-    }
-
-
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 65 // 각 셀의 높이 조정
-    }
-
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 0
-    }
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-
 }
