f9b9b8670f9ed07a3e893b92c955f5874660fc27
ya_e_un <nyeshinee0525@naver.com>
2023-10-04 17:23:05 +0900

    feat:비빌번호 찾기 UI구현

--- RF/Views/SignIn/SearchPWResultViewController.swift
+++ RF/Views/SignIn/SearchPWResultViewController.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 import SnapKit
 
-final class SearchPWResultViewController: UIViewController {
+final class SearchPWResultViewController: UIViewController, UITextFieldDelegate {
     
     private lazy var leftButton: UIBarButtonItem = {
         let button = UIBarButtonItem(title: "비밀번호 찾기", style: .done, target: self, action: nil)
@@ -27,27 +27,16 @@ final class SearchPWResultViewController: UIViewController {
         return nameLabel
     }()
     
-    // 변경된 부분: PasswordTextField 사용
-    private lazy var PWField: PasswordTextField = {
-        let nameField = PasswordTextField()
-        nameField.text = "비밀번호를 새로 변경해주세요."
-        nameField.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        nameField.textColor = TextColor.first.color
-        return nameField
-    }()
     
-    // 변경된 부분: PasswordTextField의 eyeButton 사용
-    private lazy var eyeButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.setImage(UIImage(named: "eye_icon"), for: .normal)
-        button.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
-        button.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
-        
-        PWField.rightView = button
-        PWField.rightViewMode = .always
-        
-        return button
+    private lazy var pwTextField: PasswordTextField = {
+        var view = PasswordTextField()
+        view.placeholder = "비밀번호를 새로 변경해 주세요"
+        view.delegate = self
+        view.font = .systemFont(ofSize: 14)
+        view.setColor(TextColor.first.color)
+        view.setButtonColor(TextColor.secondary.color)
+        view.borderStyle = UITextField.BorderStyle.none
+        return view
     }()
     
     private lazy var PWCheckLabel: UILabel = {
@@ -59,12 +48,15 @@ final class SearchPWResultViewController: UIViewController {
         return mailLabel
     }()
     
-    private lazy var PWCheckField: UITextField = {
-        let mailField = UITextField()
-        mailField.text = "새로운 비밀번호를 다시 입력해주세요."
-        mailField.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        mailField.textColor = TextColor.first.color
-        return mailField
+    private lazy var pwTextCheckField: PasswordTextField = {
+        var view = PasswordTextField()
+        view.placeholder = "새로운 비밀번호를 다시 입력해주세요."
+        view.delegate = self
+        view.font = .systemFont(ofSize: 14)
+        view.setColor(TextColor.first.color)
+        view.setButtonColor(TextColor.secondary.color)
+        view.borderStyle = UITextField.BorderStyle.none
+        return view
     }()
 
     @objc private lazy var nextButton: UIButton = {
@@ -85,34 +77,18 @@ final class SearchPWResultViewController: UIViewController {
         navigationItem.leftBarButtonItem = leftButton
         navigationController?.navigationBar.tintColor = TextColor.first.color
         view.backgroundColor = .white
+    
         
-        // searchIDresult UI로 화면 전환
-        nextButton.addTarget(self, action: #selector(getter: nextButton), for: .touchUpInside)
-        
-        // 변경된 부분: togglePasswordVisibility 함수의 target을 self로 지정
-        eyeButton.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
         
         addSubviews()
     }
-    
-    // 변경된 부분: togglePasswordVisibility 함수
-    @objc private func togglePasswordVisibility() {
-        PWField.isSecureTextEntry.toggle()
-        let imageName = PWField.isSecureTextEntry ? "eye_icon" : "eye_slash_icon"
-        eyeButton.setImage(UIImage(named: imageName), for: .normal)
-    }
 
-    //// 나예은_searchIDresult UI로 화면 전환
-    @objc func findIdButtonTapped() {
-           let searchIDVC = SearchIDResultViewController()
-           navigationController?.pushViewController(searchIDVC, animated: true)
-       }
     
     private func addSubviews() {
         view.addSubview(PWLabel)
-        view.addSubview(PWField)
+        view.addSubview(pwTextField)
         view.addSubview(PWCheckLabel)
-        view.addSubview(PWCheckField)
+        view.addSubview(pwTextCheckField)
         view.addSubview(nextButton)
         
         configureConstraints()
@@ -127,19 +103,19 @@ final class SearchPWResultViewController: UIViewController {
         }
         
         // 비밀번호를 새로 변경해주세요
-        PWField.snp.makeConstraints { make in
+        pwTextField.snp.makeConstraints { make in
             make.top.equalTo(PWLabel.snp.bottom).offset(4)
             make.leading.trailing.equalToSuperview().inset(19)
         }
         
         // 비밀번호 확인
         PWCheckLabel.snp.makeConstraints { make in
-            make.top.equalTo(PWField.snp.bottom).offset(16)
+            make.top.equalTo(pwTextField.snp.bottom).offset(16)
             make.leading.trailing.equalToSuperview().inset(19)
         }
         
         // 새로운 비밀번호를 다시 입력해주세요
-        PWCheckField.snp.makeConstraints { make in
+        pwTextCheckField.snp.makeConstraints { make in
             make.top.equalTo(PWCheckLabel.snp.bottom).offset(4)
             make.leading.trailing.equalToSuperview().inset(19)
         }
